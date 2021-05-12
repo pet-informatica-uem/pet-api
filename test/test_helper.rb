1ba738ae2e5a5bb078e9adb7452c210f52d63814
bfsgr <bfsgr.code@protonmail.com>
2021-05-12 17:17:21 -0300

    setup test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,28 @@
+require 'simplecov'
+require 'simplecov_json_formatter'
+require 'simplecov-lcov'
+
+SimpleCov.start 'rails' do
+  formatter SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::JSONFormatter,
+                                                      SimpleCov::Formatter::LcovFormatter,
+                                                      SimpleCov::Formatter::HTMLFormatter])
+end
+
 ENV['RAILS_ENV'] ||= 'test'
-require_relative "../config/environment"
-require "rails/test_help"
+require_relative '../config/environment'
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
+
+  parallelize_setup do |worker|
+    SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+  end
+
+  parallelize_teardown do
+    SimpleCov.result
+  end
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
